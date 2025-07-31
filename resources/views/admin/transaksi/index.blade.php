2ef712846b8c528f5268f2197e1898f0ce0b6737
yulita123456 <yulitanurfathin@gmail.com>
2025-07-31 13:16:32 +0700

    perubahan bg

--- resources/views/admin/transaksi/index.blade.php
+++ resources/views/admin/transaksi/index.blade.php
@@ -90,7 +90,7 @@
 
     <!-- OPSIONAL: Summary ringkas -->
     <div class="grid grid-cols-2 sm:grid-cols-4 gap-2 mb-4 text-xs">
-        <div class="bg-green-100 rounded-xl py-3 text-center">
+        <div class="bg-green-50 rounded-xl py-3 text-center">
             <div class="font-semibold text-green-900">{{ $transactions->total() }}</div>
             <div class="text-green-500">Total Transaksi</div>
         </div>
