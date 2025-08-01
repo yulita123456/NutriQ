3e3f6337f77084cf12188c3fdefc916342bec765
yulita123456 <yulitanurfathin@gmail.com>
2025-08-02 00:00:00 +0700

    symliiiiiinkkk 6

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-if [ ! -L /var/www/html/public/storage ]; then
-    php artisan storage:link
-fi
-
+# Jalankan migrasi database
 php artisan migrate --force
+
+# Hapus cache yang mungkin usang
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
+
+# Jalankan cache baru
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
+# Jalankan Apache
 exec apache2-foreground
