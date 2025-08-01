c404d2e7d6d98384df4eae48ff4cc4e8ba8be3de
yulita123456 <yulitanurfathin@gmail.com>
2025-08-02 00:21:39 +0700

    symliiiiiinkkk 10

--- config/filesystems.php
+++ config/filesystems.php
@@ -1,63 +1,63 @@
-    <?php
+<?php
 
-    return [
+return [
 
-        /*
-        |--------------------------------------------------------------------------
-        | Default Filesystem Disk
-        |--------------------------------------------------------------------------
-        */
+    /*
+    |--------------------------------------------------------------------------
+    | Default Filesystem Disk
+    |--------------------------------------------------------------------------
+    */
 
-        'default' => env('FILESYSTEM_DISK', 'public'),
+    'default' => env('FILESYSTEM_DISK', 'public'),
 
-        /*
-        |--------------------------------------------------------------------------
-        | Filesystem Disks
-        |--------------------------------------------------------------------------
-        */
+    /*
+    |--------------------------------------------------------------------------
+    | Filesystem Disks
+    |--------------------------------------------------------------------------
+    */
 
-        'disks' => [
+    'disks' => [
 
-            'local' => [
-                'driver' => 'local',
-                'root' => storage_path('app/private'),
-                'serve' => true,
-                'throw' => false,
-                'report' => false,
-            ],
-
-            'public' => [
-                'driver' => 'local',
-                'root' => storage_path('app/public'),
-                'url' => env('APP_URL') . '/storage',
-                'visibility' => 'public',
-                'throw' => false,
-                'report' => false,
-            ],
-
-            's3' => [
-                'driver' => 's3',
-                'key' => env('AWS_ACCESS_KEY_ID'),
-                'secret' => env('AWS_SECRET_ACCESS_KEY'),
-                'region' => env('AWS_DEFAULT_REGION'),
-                'bucket' => env('AWS_BUCKET'),
-                'url' => env('AWS_URL'),
-                'endpoint' => env('AWS_ENDPOINT'),
-                'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-                'throw' => false,
-                'report' => false,
-            ],
-
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app/private'),
+            'serve' => true,
+            'throw' => false,
+            'report' => false,
         ],
 
-        /*
-        |--------------------------------------------------------------------------
-        | Symbolic Links
-        |--------------------------------------------------------------------------
-        */
-
-        'links' => [
-            public_path('storage') => storage_path('app/public'),
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'public',
+            'throw' => false,
+            'report' => false,
         ],
 
-    ];
+        's3' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+            'report' => false,
+        ],
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Symbolic Links
+    |--------------------------------------------------------------------------
+    */
+
+    'links' => [
+        public_path('storage') => storage_path('app/public'),
+    ],
+
+];
